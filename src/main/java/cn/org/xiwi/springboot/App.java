5bfab53a29400ccbf37cfb90fe115d9fc3d05213
xiwi92 <616340233@qq.com>
2017-04-26 15:18:25 +0800

    update

--- src/main/java/cn/org/xiwi/springboot/App.java
+++ src/main/java/cn/org/xiwi/springboot/App.java
@@ -1,12 +1,14 @@
 package cn.org.xiwi.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @Configuration
+//@EnableAutoConfiguration
 @ImportResource(locations={"classpath:config/application-mina.xml"})
 public class App 
 {
